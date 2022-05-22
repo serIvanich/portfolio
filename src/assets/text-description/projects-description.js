13c1c3e4683bad8c718b10b6d10026ec0e971086
serIvanich <serivan1108@gmail.com>
2022-05-22 21:19:55 +0300

    changed text in projects description

--- src/assets/text-description/projects-description.js
+++ src/assets/text-description/projects-description.js
@@ -1,10 +1,13 @@
 
 export const projectsDescription = {
-  descriptionTodoList: 'To add TypeScript to an existing Create React App project, first install.',
-  descriptionionSocialNetwork: 'CSS Modules let you use the same CSS class name',
-  descriptionCards: 'let you use the same cards',
-  descriptionCounter: 'counter for use something',
-  descriptionShoppingCart: 'you can add and delete product that you need',
-  descriptionGradients: 'create your own gragients',
-  descriptionMyNotebook: 'you can have under controll your matters',
-}
+  descriptionTodoList: `That's a learning project, which I've done on my classes of React/TS developing. 
+  In the project you can create your own list of actions and have control over them`,
+  descriptionSocialNetwork: `in work...mobile version is not adaptive...That's social network for persons who learning programing with online course "Samuray's way"`,
+  descriptionCards: `in work...mobile version is not adaptive...This project we did in team fron three persons.
+  I've been own of project in github and did merge for project`,
+  descriptionCounter: `That's a pet project where you can increment number from min to max border setting`,
+  descriptionShoppingCart: `This's my pet-project where you can choose a project for buying in the shopping cart
+  and write down your data`,
+  descriptionGradients: `That project is giving pissibility to create some color's gradients and change them if you want`,
+  descriptionMyNotebook: `This's my test project where user can have under controll his schedule`,
+} 
