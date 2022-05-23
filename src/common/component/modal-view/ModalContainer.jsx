6fc78beb7d2e49563608a6ddacff962147ea8189
serIvanich <serivan1108@gmail.com>
2022-05-23 20:16:36 +0300

    changed view of project description

--- src/common/component/modal-view/ModalContainer.jsx
+++ src/common/component/modal-view/ModalContainer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './ModalContainer.module.scss'
 
-export const ModalContainer = ({children}) => {
+export const ModalContainer = ({children, onClickModalContainer}) => {
+
+  const onClick = () => {
+    if (onClickModalContainer) {
+      onClickModalContainer()
+    }
+  }
 
   return (
-    <div className={s.modalContainer}>
+    <div className={s.modalContainer} onClick={onClick}>
 
       {children}
     </div>
--- src/features/projects/project/Project.jsx
+++ src/features/projects/project/Project.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react'
-
+import {ModalContainer} from '../../../common/component/modal-view/ModalContainer'
 import s from './Project.module.scss'
 
 export const Project = ({projectTitle, projectDescription, linkTo, style}) => {
     const [showDescription, setShowDescription] = useState(false)
-    const onClickDescription = () => {
-        setShowDescription(!showDescription)
+    const openDescription = () => {
+        console.log('open')
+        setShowDescription(true)
     }
-    const descriptionStyles = {
-        display:showDescription? 'inline-block' : 'none'
+    const closeDescription = () => {
+        console.log('close')
+        setShowDescription(false) 
     }
+    // const descriptionStyles = {
+    //     display:showDescription? 'inline-block' : 'none'
+    // }
     return (
         <div className={s.project}>
             <div className={s.projectPictureBlock} style={style}>
                 <a href={linkTo} className={s.projectLink}>LINK</a>
             </div>
-            <h2 className={s.descriptionTitle} onClickCapture={onClickDescription}>{projectTitle}</h2>
-            <span className={s.projectDescription} style={descriptionStyles}>{projectDescription}</span>
+            <h2 className={s.projectTitleDescription} onClickCapture={openDescription}>{projectTitle}</h2>
+            { showDescription && <ModalContainer onClickModalContainer={closeDescription}>
+                <div className={s.projectTextDescription}>
+                    {projectDescription}        
+                </div>
+            </ModalContainer>}
+            {/* <span className={s.projectDescription} style={descriptionStyles}>{projectDescription}</span> */}
         </div>
     )
 }
