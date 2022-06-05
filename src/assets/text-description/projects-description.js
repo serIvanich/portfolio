a7987feac903613c68096ce2897aa50029b407e3
serIvanich <serivan1108@gmail.com>
2022-06-05 11:16:34 +0300

    changed different projects description

--- src/assets/text-description/projects-description.js
+++ src/assets/text-description/projects-description.js
@@ -5,17 +5,19 @@ export const projectsDescription = {
     This project was developed with TypeScript to had the best controlle under project's data and functions,
     I used Material UI to had a formated and adaptive page on the user's screen 
     Redux Toolkit is helping to work with state of project, create, change, delete data,
-    working with project's server(get, put, update, delete with help of Axios).
+    working with project's server (get, put, update, delete with help of Axios).
     For working with project's form i used formik who is an object from  hook useFormik from formik...
     Also project have different tests, for testing functions and components 
     with help react testing library, jest, Storybook.
     `,
-  descriptionSocialNetwork: `in work...mobile version is not adaptive...
-  That's social network for persons who learning programing with online course "Samuray's way"
+  descriptionSocialNetwork: `in work...mobile version is not work...
+  This is a project of a social network that is created and used by people who learn to program in IT-Incubator classes 
+  and a free online course "Samurai Way"  
   `,
-  descriptionCards: `in work...mobile version is not adaptive...
-  This project we did in team fron three persons.
-  I've been own of project in github and did merge for project
+  descriptionCards: `in work...mobile version is not work...
+  We worked on this project with a three-person team.
+  I had a repository in Githab where we kept him.Each had project tasks. 
+  After they were completed, we kept our branches, and then I merged them.
   `,
   descriptionCounter: `This is a pet project where the user increases  number  by one from the minimum to the maximum limit 
   which he can change.Application was created using React/TypeScript technology. 
@@ -34,6 +36,10 @@ export const projectsDescription = {
   SASS helps us to have easier work with components' styles.
   There's a test for reducer in this project. 
   `,
-  descriptionMyNotebook: `This's my test project where user can have under controll his schedule
+  descriptionMyNotebook: `This project was created for users who want to control their affairs, 
+  keeping information on their details and progress. User can create new or delete or modify his notes  
+  I used React/JS to create the project. 
+  Also React-Redux, formik, react-thunk, SASS helped me to write this code more easily and conveniently. 
+  There are  tests for control functions reducer and thunk in the project
   `,
 } 
